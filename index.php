87c0295cca32d79f3d04e903d20c337f4301df53
Miss Cissy <hanirizo@gmail.com>
2017-11-28 01:45:20 +0700

    c

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+require_once ('vendor/autoload.php');
+use \Statickidz\GoogleTranslate;
+
+$source = 'en';
+$target = 'es';
+$text = $_GET['q'];
+
+$trans = new GoogleTranslate();
+$result = $trans->translate($source, $target, $text);
+echo $result.'<br/>';
+
+$result = $trans->translate($source, 'de', $text);
+echo $result.'<br/>';
+
+$result = $trans->translate($source, 'it', $text);
+echo $result.'<br/>';
+
+$result = $trans->translate($source, 'id', $text);
+echo $result.'<br/>';
+
+$result = $trans->translate($source, 'ru', $text);
+echo $result.'<br/>';
+
+$result = $trans->translate($source, 'ja', $text);
+echo $result.'<br/>';
+
+?>
